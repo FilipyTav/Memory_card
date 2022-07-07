77c76b78e0af90c6af385a03af2550e31852d549
BiAniTy <drayttc@gmail.com>
2022-07-07 20:47:39 -0300

    Creates module for the cards and stylesheet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import "./styles/App.css";
+import { useEffect, useState } from "react";
+import Card from "./components/Card";
+
 const App = () => {
-    return <div>hey</div>;
+    return (
+        <div className="main_container">
+            <header className="header">
+                <span className="header_txt">Memory Card Game</span>
+            </header>
+            <div className="main_content"></div>
+        </div>
+    );
 };
 
 export default App;
