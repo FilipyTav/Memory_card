7cdd5da7b84e325867c9c14124e37db9ab49974c
BiAniTy <drayttc@gmail.com>
2022-07-09 15:04:38 -0300

    Adds comments

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import { useEffect, useState } from "react";
 import Card from "./components/Card";
 
 const App = () => {
+    // All the cards and their content
     const [all_cards, set_all_cards] = useState([]);
+
+    // Array of cards that were already selected
     const [cards_already_selected, set_cards_already_selected] = useState([]);
+
+    // Score and highscore
     const [score_count, set_score_count] = useState(0);
     const [highscore_count, set_highscore_count] = useState(0);
 
+    // Content to be distributed randomly to all cards
     let cards_content = [];
 
+    // Adds the content to the array
     const populate_cards_content = () => {
         for (let i = 0; i < 12; i++) {
             cards_content.push(i);
         }
     };
 
+    // Random integer between min and max, both inclusive
+    // Used to select a content for the cards
     const get_random_int = (min, max) =>
         Math.floor(Math.random() * (max - min + 1)) + min;
 
+    // Selectes a random content and removes the selected content from the array
     const choose_random_content = () => {
         const index = get_random_int(0, cards_content.length - 1);
 
@@ -27,8 +37,10 @@ const App = () => {
         return removed;
     };
 
+    // Creates the Card elements and adds them to all_cards
     const create_cards = () => {
         for (let i = 0; i < 12; i++) {
+            // Content to be assigned
             const content = choose_random_content();
 
             set_all_cards((prev_cards) => [
@@ -38,6 +50,7 @@ const App = () => {
         }
     };
 
+    // Adds the content to the cards and render them
     const initialize = () => {
         populate_cards_content();
 
@@ -47,25 +60,34 @@ const App = () => {
     useEffect(() => {
         initialize();
 
+        // The cards are reset after each render if the score changes
         return () => {
             set_all_cards([]);
             cards_content = [];
         };
     }, [score_count]);
 
+    // Selects a card when it is clicked
     const select_card = (e) => {
         const selected = e.target.textContent;
 
+        // The else if is here because the set function of useState is async (delayed)
+
+        // If the card has not been selected previously and it is not the last card available
         if (
             !cards_already_selected.includes(selected) &&
             score_count + 1 < 12
         ) {
+            // Considers the card as selected
             set_cards_already_selected((prev_cards) => [
                 ...prev_cards,
                 selected,
             ]);
+
+            // Adds 1 to the score
             set_score_count((prev_score) => prev_score + 1);
         } else if (
+            // If it is the last card available
             !cards_already_selected.includes(selected) &&
             score_count + 1 === 12
         ) {
@@ -74,13 +96,19 @@ const App = () => {
                 selected,
             ]);
 
+            // Sets the highscore to the maximum amount
             set_highscore_count(score_count + 1);
 
+            // Resets the score
             set_score_count(0);
+
+            // Clears the selected cards
             set_cards_already_selected([]);
         } else {
+            // Only changes the highscore if the score is higher
             if (score_count > highscore_count) set_highscore_count(score_count);
 
+            // Resets the score and the selected cards
             set_score_count(0);
             set_cards_already_selected([]);
         }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,7 @@
 const Card = (props) => {
+    // The card content and its functionality
     const { content, select_card } = props;
+
     return (
         <div className="card" onClick={select_card}>
             {content}
